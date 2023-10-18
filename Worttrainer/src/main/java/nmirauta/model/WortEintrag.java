eef9f85c086f514f2325edf1bc94b8152e5f0bd1
NatiMirauta7 <93704853+NatiMirauta7@users.noreply.github.com>
2023-10-18 15:39:51 +0200

    Worteintrag

---
+++ Worttrainer/src/main/java/nmirauta/model/WortEintrag.java
@@ -0,0 +1,44 @@
+package nmirauta.model;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+
+public class WortEintrag {
+    private String wort;
+    private String url;
+
+    public WortEintrag(String wort, String url) {
+        setWort(wort);
+        setUrl(url);
+    }
+
+    public String getWort() {
+        return wort;
+    }
+
+    public boolean setWort(String wort) {
+        if(wort.length() > 1) {
+            this.wort = wort;
+            return true;
+        }
+        return false;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public boolean setUrl(String url) {
+        String urlString = url;
+        try {
+            URL checkedUrl = new URL(urlString);
+            this.url = checkedUrl.toString();
+        } catch (MalformedURLException e) {
+            System.out.println("Die URL ist ungültig: " + urlString);
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
